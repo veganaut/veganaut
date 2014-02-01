71d63d5b2970da8d3cc51cc971ba4a0c0b48efa9
Tobias Leugger <leugger.tobias@web.de>
2014-02-01 23:42:45 +0100

    Fixing the e2e tests and expanding them a bit

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -3,43 +3,43 @@
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 /* global describe, beforeEach, it, expect, browser, element */
 describe('my app', function() {
-
     beforeEach(function() {
         browser().navigateTo('/app/index.html');
     });
-
 
     it('should automatically redirect to /socialGraph when location hash/fragment is empty', function() {
         expect(browser().location().url()).toBe('/socialGraph');
     });
 
-
     describe('socialGraph', function() {
-
         beforeEach(function() {
             browser().navigateTo('#/socialGraph');
         });
 
-
-        it('should render socialGraph when user navigates to /socialGraph', function() {
-            expect(element('[ng-view] h2:first').text()).
-                toMatch(/Your graph/);
+        it('should render socialGraph when navigating to /socialGraph', function() {
+            expect(element('social-graph').count()).toMatch(1);
+            expect(element('social-graph svg').count()).toMatch(1);
         });
-
     });
 
-
     describe('activity', function() {
-
         beforeEach(function() {
             browser().navigateTo('#/activity');
         });
 
+        it('should redirect to /socialGraph when navigating directly to /activity', function() {
+            expect(browser().location().url()).toBe('/socialGraph');
+        });
+    });
 
-        it('should render activity when user navigates to /activity', function() {
-            expect(element('[ng-view] h2:first').text()).
-                toMatch(/Start an activity with/);
+    describe('login', function() {
+        beforeEach(function() {
+            browser().navigateTo('#/login');
         });
 
+        it('should render login form when navigating to /login', function() {
+            expect(element('[ng-view] form').count()).toBe(1);
+            expect(element('[ng-view] form input[type=password]').count()).toBe(1);
+        });
     });
 });
