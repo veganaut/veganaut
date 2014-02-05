eef91e333abfe55e51967eb8afb22a3b3e02e471
Tobias Leugger <leugger.tobias@web.de>
2014-02-05 23:28:47 +0100

    Fixing Backend service tests

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -18,9 +18,22 @@ describe('service', function() {
             $provide.value('backendUrl', '');
         }));
 
-        it('should have a submit method', inject(function($httpBackend, backend) {
+        it('should have a isLoggedIn method', inject(function($httpBackend, backend) {
+            expect(typeof backend.isLoggedIn).toBe('function');
+
+            // Should not be logged in by default
+//            expect(backend.isLoggedIn()).toBe(false);
+            // TODO: this doesn't work because it stores the sid in the session storage
+            // Need to mock the sessionStorage somehow
+        }));
+
+        it('should have a login method', inject(function($httpBackend, backend) {
             // Set up the http backend mock
-            $httpBackend.expectPOST('/session', {email: 'e@mail.com', password: 'word'}).respond();
+            $httpBackend.expectPOST('/session', {email: 'e@mail.com', password: 'word'})
+                .respond({
+                    sessionId: 'test-session-d'
+                })
+            ;
 
             expect(typeof backend.login).toBe('function');
 
@@ -32,6 +45,11 @@ describe('service', function() {
             expect(typeof req.then).toBe('function');
             expect(typeof req.success).toBe('function');
             expect(typeof req.error).toBe('function');
+
+            // Should be logged in now
+            expect(backend.isLoggedIn()).toBe(true);
         }));
+
+        // TODO: test other backend methods
     });
 });
