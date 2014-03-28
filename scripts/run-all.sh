074bc541d1fcc3e0a7e6930cf8fcd7ab35f00f31
Nathan Gass <gass@search.ch>
2014-03-28 23:47:49 +0100

    Add script to run all background processes.

---
+++ scripts/run-all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p log
+
+function cleanUp() {
+  kill `cat log/mongodb.pid`
+}
+
+trap cleanUp EXIT
+
+mkdir -p mongodb
+mongod --dbpath mongodb --pidfilepath log/mongodb.pid > log/mongodb.log 2>&1 &
+
+scripts/web-server.js > log/webserver.log 2>&1 &
+
+cd ../monkey-tail
+supervisor app.js > ../monkey-face/log/app.log 2>&1 &
+
+cd ../monkey-face
+
+echo "Go to http://localhost:8000/app/index.html"
+wait
+
