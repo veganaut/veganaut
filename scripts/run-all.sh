8b78dde7d1d12616d43b3b75ce79b4ea1d72d909
Tobias Leugger <leugger.tobias@web.de>
2014-03-29 11:20:25 +0100

    Only starting monod in run-all.sh if not already running.

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -6,12 +6,17 @@ function cleanUp() {
   kill `cat log/mongodb.pid`
 }
 
-trap cleanUp EXIT
+if ! pgrep "mongod" > /dev/null; then
+    echo "Staring mongod"
+    trap cleanUp EXIT
 
-mkdir -p mongodb
-mongod --dbpath mongodb --pidfilepath log/mongodb.pid > log/mongodb.log 2>&1 &
+    mkdir -p mongodb
+    mongod --dbpath mongodb --pidfilepath log/mongodb.pid > log/mongodb.log 2>&1 &
+fi
 
 scripts/web-server.js > log/webserver.log 2>&1 &
+
+node_modules/.bin/karma start config/karma-unit.conf.js > log/karma.log 2>&1 &
 
 cd ../monkey-tail
 supervisor app.js > ../monkey-face/log/app.log 2>&1 &
