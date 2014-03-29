9dc56352c22d70cb6bfd7ca335af335a47f342b6
Nathan Gass <gass@search.ch>
2014-03-29 13:38:28 +0100

    Replace test-all.sh with script which runs every test and generates a summary.

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-set -ex
-
-function cleanUp() {
-  kill $WEBSERVER_PID
-}
-
-trap cleanUp EXIT
-
 # Define reasonable set of browsers in case we are running manually from commandline
-if [[ -z "$BROWSERS" ]]
-then
+if [[ -z "$BROWSERS" ]]; then
   BROWSERS="Firefox"
 fi
 
-if [[ -z "$BROWSERS_E2E" ]]
-then
-  BROWSERS_E2E="Firefox"
-fi
+./node_modules/.bin/protractor config/protractor.conf.js | tee tests.out
+./node_modules/karma/bin/karma start config/karma-unit.conf.js --single-run --browsers $BROWSERS | tee -a tests.out
 
-ROOT_DIR=`dirname $0`/..
+cd ../monkey-tail
+./node_modules/.bin/jasmine-node test/e2e/     | tee -a ../monkey-face/tests.out
+./node_modules/.bin/jasmine-node test/models/  | tee -a ../monkey-face/tests.out
 
-cd $ROOT_DIR
-npm install
+cd ../monkey-face
+TESTS=0; ASSERTIONS=0; FAILURES=0; SKIPPED=0;
+for line in `grep -P '^(\x1b\[\d+(;\d+)*\w)?\d+ tests?,' tests.out | sed 's/ /_/g'`; do
+	NUMBERS=(`echo $line | grep -o -P '\d+'`)
+	TESTS=$(($TESTS + ${NUMBERS[1]}))
+	ASSERTIONS=$(($ASSERTIONS + ${NUMBERS[2]}))
+	FAILURES=$(($FAILURES + ${NUMBERS[3]}))
+	SKIPPED=$(($SKIPPED + ${NUMBERS[3]}))
+done;
 
-./scripts/web-server.js > /dev/null &
-WEBSERVER_PID=$!
-
-
-./node_modules/karma/bin/karma start config/karma-unit.conf.js --single-run --browsers $BROWSERS
-./node_modules/karma/bin/karma start config/karma-e2e.conf.js --browsers $BROWSERS_E2E
+echo Summary:
+if [[ $FAILURES -eq 0 ]]; then
+	tput setaf 2
+else
+	tput setaf 1
+fi;
+echo $TESTS tests, $ASSERTIONS assertions, $FAILURES failures, $SKIPPED skipped
+tput sgr0
