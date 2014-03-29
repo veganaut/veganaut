cbea019180da446d36026fe860c03861c3105d08
Nathan Gass <gass@search.ch>
2014-03-29 15:58:33 +0100

    Remove wrong summary again.

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -21,21 +21,3 @@ cd ../monkey-tail
 ./node_modules/.bin/jasmine-node test/e2e/     | tee -a ../monkey-face/tests.out
 ./node_modules/.bin/jasmine-node test/models/  | tee -a ../monkey-face/tests.out
 
-cd ../monkey-face
-TESTS=0; ASSERTIONS=0; FAILURES=0; SKIPPED=0;
-for line in `grep -P '^(\x1b\[\d+(;\d+)*\w)?\d+ tests?,' tests.out | sed 's/ /_/g'`; do
-	NUMBERS=(`echo $line | grep -o -P '\d+'`)
-	TESTS=$(($TESTS + ${NUMBERS[1]}))
-	ASSERTIONS=$(($ASSERTIONS + ${NUMBERS[2]}))
-	FAILURES=$(($FAILURES + ${NUMBERS[3]}))
-	SKIPPED=$(($SKIPPED + ${NUMBERS[3]}))
-done;
-
-echo Summary:
-if [[ $FAILURES -eq 0 ]]; then
-	tput setaf 2
-else
-	tput setaf 1
-fi;
-echo $TESTS tests, $ASSERTIONS assertions, $FAILURES failures, $SKIPPED skipped
-tput sgr0
