e3a8ace68c3a9585e0394fc83a032edf407d416d
Tobias Leugger <leugger.tobias@web.de>
2014-07-23 23:46:36 +0200

    Adding a few test markers to the map

--- app/veganaut/map/MapCtrl.js
+++ app/veganaut/map/MapCtrl.js
@@ -12,6 +12,25 @@
                 lng: 7.451,
                 zoom: 14
             };
+
+            $scope.markers = {
+                test1: {
+                    lat: 46.949,
+                    lng: 7.451
+                },
+                test2: {
+                    lat: 46.945,
+                    lng: 7.456
+                },
+                test3: {
+                    lat: 46.95,
+                    lng: 7.459
+                },
+                test4: {
+                    lat: 46.94,
+                    lng: 7.44
+                }
+            };
         }
     ]);
 })(window.monkeyFace.controllersModule);
