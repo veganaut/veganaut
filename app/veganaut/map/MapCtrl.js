493c92474268f00ed4df8cf8ab6da1f37e5f84c8
Tobias Leugger <leugger.tobias@web.de>
2014-07-23 23:54:45 +0200

    Adding messages to the markers

--- app/veganaut/map/MapCtrl.js
+++ app/veganaut/map/MapCtrl.js
@@ -16,19 +16,31 @@
             $scope.markers = {
                 test1: {
                     lat: 46.949,
-                    lng: 7.451
+                    lng: 7.451,
+                    message: 'Some place'
                 },
                 test2: {
                     lat: 46.945,
-                    lng: 7.456
+                    lng: 7.456,
+                    message: 'Some other place'
                 },
                 test3: {
                     lat: 46.95,
-                    lng: 7.459
+                    lng: 7.459,
+                    message: 'Great place'
                 },
                 test4: {
                     lat: 46.94,
-                    lng: 7.44
+                    lng: 7.44,
+                    message: 'Soon to be great place'
+                }
+            };
+
+            $scope.focusedMarker = function() {
+                for (var markerName in $scope.markers) {
+                    if ($scope.markers.hasOwnProperty(markerName) && $scope.markers[markerName].focus === true) {
+                        return $scope.markers[markerName];
+                    }
                 }
             };
         }
