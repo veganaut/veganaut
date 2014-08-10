efaa2e018b94a01b3bf8f7edb867ca12df456c9b
Tobias Leugger <leugger.tobias@web.de>
2014-08-10 23:10:02 +0200

    Adding angular-mocks as a bower dev dependency

--- config/karma-unit.conf.js
+++ config/karma-unit.conf.js
@@ -8,7 +8,7 @@ module.exports = function(config) {
         files: [
             'app/lib/angular/angular.js',
             'app/lib/angular-route/angular-route.js',
-            'test/lib/angular/angular-mocks.js',
+            'app/lib/angular-mocks/angular-mocks.js',
             'app/veganaut/app.js',
             'app/components/**/*.js',
             'app/veganaut/**/*.js',
