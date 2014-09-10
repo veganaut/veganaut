e57adbd20beef9c2c7d2ada4ba76fcd195283d55
Tobias Leugger <leugger.tobias@web.de>
2014-09-10 21:15:08 +0200

    Bottom box is now top-box and map fills all available space. Moar map! Also no more zoom controlls on the map. No need for that on mobile / touch devices. Also otherwise of dubious use.

--- app/veganaut/config.js
+++ app/veganaut/config.js
@@ -7,33 +7,31 @@
 
     // Different possible map tiles
     var osmAttribution = '© <a href="http://www.openstreetmap.org/copyright" target="_blank" >OpenStreetMap</a> Data';
+    var mapDefaults = {
+        zoomControl: false
+    };
+
     // TODO: add terms or licence link, see http://wiki.openstreetmap.org/wiki/Legal_FAQ
     // Mapbox tiles: only for production
-//    module.value('mapDefaults', {
-//        tileLayer: 'https://{s}.tiles.mapbox.com/v3/toebu.ilh4kll0/{z}/{x}/{y}.png',
-//        tileLayerOptions: {
-//            attribution: osmAttribution + ' | <a href="https://www.mapbox.com/about/maps/" target="_blank">Mapbox</a> Tiles'
-//        }
-//    });
+    mapDefaults.tileLayer = 'https://{s}.tiles.mapbox.com/v3/toebu.ilh4kll0/{z}/{x}/{y}.png';
+    mapDefaults.tileLayerOptions = {
+        attribution: osmAttribution + ' | <a href="https://www.mapbox.com/about/maps/" target="_blank">Mapbox</a> Tiles'
+    };
 
     // Stamen Toner tiles
-//    module.value('mapDefaults', {
-//        tileLayer: 'http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png',
-//        tileLayerOptions: {
-//            subdomains: 'abcd',
-//            attribution: osmAttribution + ' | <a href="http://stamen.com/" target="_blank">Stamen</a> Tiles'
-//        }
-//    });
+    mapDefaults.tileLayer = 'http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png';
+    mapDefaults.tileLayerOptions = {
+        attribution: osmAttribution + ' | <a href="http://stamen.com/" target="_blank">Stamen</a> Tiles'
+    };
 
     // Mapquest tiles
-    module.value('mapDefaults', {
-        tileLayer: 'http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg',
-        tileLayerOptions: {
-            subdomains: '1234',
-            attribution: osmAttribution + ' | <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> Tiles'
-        }
-    });
+    mapDefaults.tileLayer = 'http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg';
+    mapDefaults.tileLayerOptions = {
+        subdomains: '1234',
+        attribution: osmAttribution + ' | <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> Tiles'
+    };
 
+    module.value('mapDefaults', mapDefaults);
 
     // Whether to use html5Mode
     module.constant('useHtml5Mode', true);
