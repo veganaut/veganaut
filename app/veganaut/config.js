89cf433a09ee13c6e475d83bc297bec305896744
Tobias Leugger <leugger.tobias@web.de>
2014-09-11 20:44:12 +0200

    Commenting out map tiles that aren't used.

--- app/veganaut/config.js
+++ app/veganaut/config.js
@@ -13,16 +13,16 @@
 
     // TODO: add terms or licence link, see http://wiki.openstreetmap.org/wiki/Legal_FAQ
     // Mapbox tiles: only for production
-    mapDefaults.tileLayer = 'https://{s}.tiles.mapbox.com/v3/toebu.ilh4kll0/{z}/{x}/{y}.png';
-    mapDefaults.tileLayerOptions = {
-        attribution: osmAttribution + ' | <a href="https://www.mapbox.com/about/maps/" target="_blank">Mapbox</a> Tiles'
-    };
+//    mapDefaults.tileLayer = 'https://{s}.tiles.mapbox.com/v3/toebu.ilh4kll0/{z}/{x}/{y}.png';
+//    mapDefaults.tileLayerOptions = {
+//        attribution: osmAttribution + ' | <a href="https://www.mapbox.com/about/maps/" target="_blank">Mapbox</a> Tiles'
+//    };
 
     // Stamen Toner tiles
-    mapDefaults.tileLayer = 'http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png';
-    mapDefaults.tileLayerOptions = {
-        attribution: osmAttribution + ' | <a href="http://stamen.com/" target="_blank">Stamen</a> Tiles'
-    };
+//    mapDefaults.tileLayer = 'http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png';
+//    mapDefaults.tileLayerOptions = {
+//        attribution: osmAttribution + ' | <a href="http://stamen.com/" target="_blank">Stamen</a> Tiles'
+//    };
 
     // Mapquest tiles
     mapDefaults.tileLayer = 'http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg';
