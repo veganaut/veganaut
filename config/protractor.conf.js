11e085bf0abf2e9a349542939ddd2913c10493bc
Tobias Leugger <leugger.tobias@web.de>
2014-09-14 20:28:41 +0200

    Removing the suites from protractor tests that just run individual files. One can use --specs=... now for that.

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -6,21 +6,6 @@ exports.config = {
 //        'browserName': 'firefox'
     },
 
-    // Spec patterns are relative to the location of this config.
-    // Suites may be used. When run without a command line parameter,
-    // all suites will run. If run with --suite=frontPage, only the patterns matched
-    // by that suite will run.
-    suites: {
-        full: '../test/e2e/**/*Test.js',
-        authentication: '../test/e2e/authenticationTest.js',
-        frontPage: '../test/e2e/frontPageTest.js',
-        map: '../test/e2e/mapTest.js',
-        profile: '../test/e2e/profileTest.js',
-        referenceCodes: '../test/e2e/referenceCodesTest.js',
-        register: '../test/e2e/registerTest.js',
-        socialGraph: '../test/e2e/socialGraphTest.js'
-    },
-
     baseUrl: 'http://127.0.0.1:8000/',
 
     // Options to be passed to Jasmine-node.
