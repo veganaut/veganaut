1bf2820f45ee718f40a7fdf9374f034593c2cce8
Jonas Wagner <jonas.wagner@epfl.ch>
2014-09-27 13:32:56 +0200

    Minor simplification of test script

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -18,6 +18,5 @@ fi
 ./node_modules/karma/bin/karma start config/karma-unit.conf.js --single-run --browsers $BROWSERS | tee -a tests.out
 
 cd ../veganaut-backend
-./node_modules/.bin/jasmine-node --captureExceptions test/e2e/     | tee -a ../veganaut/tests.out
-./node_modules/.bin/jasmine-node --captureExceptions test/models/  | tee -a ../veganaut/tests.out
+./node_modules/.bin/jasmine-node --captureExceptions test     | tee -a ../veganaut/tests.out
 
