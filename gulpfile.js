65890842f90837ceb6246a1df7f074becb0d58b7
Tobias Leugger <leugger.tobias@web.de>
2014-11-02 18:30:11 +0100

    Starting to work on a gulpfile for building the app.

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+'use strict';
+/**
+ * Gulp build file for Veganaut.
+ * The build is work in progress and is not fully functional yet.
+ */
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var less = require('gulp-less');
+
+var jsFiles = [
+    'app/components/**/*.js',
+    'app/veganaut/app.js',
+    'app/veganaut/**/*.js'
+];
+
+var jsLibFiles = [
+    'app/lib/lodash/dist/lodash.js',
+    'app/lib/jquery/dist/jquery.js',
+    'app/lib/angular/angular.js',
+    'app/lib/d3/d3.js',
+    'app/lib/leaflet/dist/leaflet-src.js',
+    'app/lib/angular-route/angular-route.js',
+    'app/lib/angular-ui-bootstrap-bower/ui-bootstrap-tpls.js',
+    'app/lib/angular-loading-bar/build/loading-bar.js',
+    'app/lib/angular-leaflet-directive/dist/angular-leaflet-directive.js',
+    'app/lib/bootstrap-tour/build/js/bootstrap-tour-standalone.js'
+];
+
+var lessFile = 'app/less/master.less';
+
+gulp.task('js', function() {
+    gulp.src(jsFiles)
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/build/'))
+    ;
+});
+
+gulp.task('jsLib', function() {
+    gulp.src(jsLibFiles)
+        .pipe(concat('lib.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/build/'))
+    ;
+});
+
+gulp.task('less', function() {
+    gulp.src(lessFile)
+        .pipe(less({
+            compress: true
+        }))
+        .pipe(rename('master.min.css'))
+        .pipe(gulp.dest('app/build/'))
+    ;
+});
+
+gulp.task('default', ['js', 'jsLib', 'less']);
