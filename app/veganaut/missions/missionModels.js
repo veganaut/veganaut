67016f8b63097f26958d1a19ebf66ffd959176c5
Tobias Leugger <leugger.tobias@web.de>
2015-06-14 21:33:34 +0200

    Fixing but that would not let you rate with product at 5 stars.

--- app/veganaut/missions/missionModels.js
+++ app/veganaut/missions/missionModels.js
@@ -294,7 +294,7 @@
         }
         if (angular.isNumber(this.outcome) &&
             this.outcome > 0 &&
-            this.outcome < this.maxRating &&
+            this.outcome <= this.maxRating &&
             this.outcome !== lastOutcomeValue)
         {
             outcome = {
@@ -306,6 +306,7 @@
     };
 
     // SetProductNameMission //////////////////////////////////////////////////
+    // TODO NOW: there should be a way to just confirm the current name
     function SetProductNameMission(location, points, lastCompletedDate, lastCompletedOutcome, product) {
         var productName = product.name;
         Mission.call(this, 'setProductName', productName, location, points, lastCompletedDate, lastCompletedOutcome, product);
