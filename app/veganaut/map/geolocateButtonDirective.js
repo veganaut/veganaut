b5d75667de25902da36bc50e28653fb36fdb3912
Tobias Leugger <leugger.tobias@web.de>
2015-09-21 20:49:48 +0200

    Improving the geo location by using the built-in leaflet method that also uses the accuracy to set the bounds. Also handling locating errors nicely and tracking the usage of the button. Refs #149

--- app/veganaut/map/geolocateButtonDirective.js
+++ app/veganaut/map/geolocateButtonDirective.js
@@ -1,68 +1,114 @@
 (function(module) {
     'use strict';
 
-    var GEOLOCATE_ZOOM = 17;
-
+    /**
+     * Maz zoom to be used when a location was found
+     * @type {number}
+     */
+    var MAX_GEOLOCATE_ZOOM = 18;
 
     /**
      * Directive for adding a geolocate button to a map.
-     * When clicked, it uses the browser's geolocation API to retrieve
-     * a location and move the map to that place.
+     * When clicked, it uses the browser's geolocation API to retrieve a location.
      */
     module.directive('vgGeolocateButton', function() {
         return {
             restrict: 'E',
-            scope: {
-                _center: '=vgMapCenter'
-            },
-            controller: ['$window', function($window) {
-                /**
-                 * Stores whether the browser supports geolocation
-                 * @type {boolean}
-                 */
-                this.supported = !!($window.navigator.geolocation);
+            scope: {},
+            controller: [
+                '$scope', '$window', '$timeout', '$translate',
+                'leafletData', 'angularPiwik', 'alertService',
+                function($scope, $window, $timeout, $translate,
+                    leafletData, angularPiwik, alertService)
+                {
+                    /**
+                     * Stores whether the browser supports geolocation
+                     * @type {boolean}
+                     */
+                    this.supported = !!($window.navigator.geolocation);
 
-                /**
-                 * Whether we are currently locating
-                 * @type {boolean}
-                 */
-                this.locating = false;
-
-                /**
-                 * Use the geolocation API to locate the user and move the map to that place
-                 * @param $event
-                 */
-                this.locate = function($event) {
-                    if (!this.supported) {
-                        // Should never happen, ui should not be rendered
-                        return;
-                    }
-
-                    // Start locating
-                    this.locating = true;
-                    $window.navigator.geolocation.getCurrentPosition(this._handleLocation.bind(this));
-                    // TODO: can one handle when access to location is denied or there is another error?
-
-                    // Blur the button
-                    $event.target.blur();
-                };
-
-                /**
-                 * Handles the returned location from the geolocation API
-                 * @param loc
-                 * @private
-                 */
-                this._handleLocation = function(loc) {
-                    // Set the center from the received location
-                    // TODO: use accuracy to zoom
-                    this._center.zoom = GEOLOCATE_ZOOM;
-                    this._center.lat = loc.coords.latitude;
-                    this._center.lng = loc.coords.longitude;
-
-                    // Done locating
+                    /**
+                     * Whether we are currently locating
+                     * @type {boolean}
+                     */
                     this.locating = false;
-                };
-            }],
+
+                    /**
+                     * Reference to the map object
+                     * @type {{}}
+                     */
+                    this._map = undefined;
+
+                    /**
+                     * Use the geolocation API to locate the user and move the map to that place
+                     * @param $event
+                     */
+                    this.locate = function($event) {
+                        if (!this.supported || !this._map) {
+                            return;
+                        }
+
+                        // Use the leaflet locate method
+                        this._map.locate();
+
+                        // Start locating
+                        this.locating = true;
+
+                        // Blur the button
+                        $event.target.blur();
+
+                        // Track usage
+                        angularPiwik.track('map.geolocation', 'start');
+                    };
+
+                    /**
+                     * Handles the returned location from leaflet locationfound event
+                     * @param location
+                     * @private
+                     */
+                    this._onLocationFound = function(location) {
+                        // Done locating
+                        this.locating = false;
+
+                        // Fit the map to the bounds
+                        this._map.fitBounds(location.bounds, {
+                            maxZoom: MAX_GEOLOCATE_ZOOM
+                        });
+
+                        // Apply after timeout (otherwise the map center is not always correctly updated)
+                        $timeout(function() {
+                            $scope.$apply();
+                        }, 0);
+
+                        // Track usage
+                        angularPiwik.track('map.geolocation', 'found');
+                    };
+
+                    /**
+                     * Handles the leaflet locationerror event
+                     * @private
+                     */
+                    this._onLocationError = function() {
+                        // Done locating
+                        this.locating = false;
+
+                        // Tell user about error
+                        alertService.addAlert($translate.instant('message.geolocate.error'), 'danger');
+
+                        // Track usage
+                        angularPiwik.track('map.geolocation', 'error');
+                    };
+
+                    // Retrieve the map
+                    leafletData.getMap().then(function(map) {
+                        this._map = map;
+
+                        // Listen to location events
+                        this._map.on('locationfound', this._onLocationFound.bind(this));
+                        this._map.on('locationerror', this._onLocationError.bind(this));
+                    }.bind(this));
+                }
+            ],
             controllerAs: 'geolocateButton',
             bindToController: true,
             templateUrl: '/veganaut/map/geolocateButton.tpl.html'
