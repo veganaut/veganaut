4dd94db152076366740ada9e10c321fae31fac66
Tobias Leugger <leugger.tobias@web.de>
2015-11-22 23:36:06 +0100

    Adding directive locationProductSummary that shows the best few products of a location.

---
+++ app/veganaut/location/locationProductSummary.js
@@ -0,0 +1,38 @@
+(function() {
+    'use strict';
+
+    /**
+     * Directive to show a summary of the products available at a location.
+     * The number of products that are shown can be set with the limitTo parameter.
+     * @returns {directive}
+     *
+     * @example
+     * <vg-location-product-summary
+     *  vg-location="location"
+     *  vg-limit-to="3">
+     * </vg-location-product-summary>
+     */
+    var locationProductSummaryDirective = function() {
+        return {
+            restrict: 'E',
+            scope: {
+                // The location for which to show a summary list of products
+                location: '=vgLocation',
+
+                // Maximum number of products to show
+                limitTo: '=vgLimitTo'
+            },
+
+            controller: [function() {
+            }],
+            controllerAs: 'locationProductSummaryVm',
+            bindToController: true,
+            templateUrl: '/veganaut/location/locationProductSummary.tpl.html'
+        };
+    };
+
+    // Expose as directive
+    angular.module('veganaut.app.location')
+        .directive('vgLocationProductSummary', [locationProductSummaryDirective])
+    ;
+})();
