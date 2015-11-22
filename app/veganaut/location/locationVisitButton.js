bb260051fa8bf140e0a8df7b4e0d40cdb7de83e4
Tobias Leugger <leugger.tobias@web.de>
2015-11-22 22:41:06 +0100

    Adding directive locationVisitButton that displays a button to get to the location details page. Also making that button's text clearer.

---
+++ app/veganaut/location/locationVisitButton.js
@@ -0,0 +1,40 @@
+(function() {
+    'use strict';
+
+    // Part of the veganaut.app.location module
+    var module = angular.module('veganaut.app.location');
+
+    /**
+     * Directive that shows a button to navigate to the given location.
+     * @returns {directive}
+     *
+     * @example
+     * <vg-location-visit-button vg-location="location"></vg-location-visit-button>
+     */
+    var locationVisitButtonDirective = function() {
+        return {
+            restrict: 'E',
+            scope: {
+                // The location to show the button for
+                location: '=vgLocation'
+            },
+
+            controller: ['$location', function($location) {
+                var vm = this;
+
+                /**
+                 * Routes to the location details page
+                 */
+                vm.visit = function() {
+                    $location.path('location/' + vm.location.id);
+                };
+            }],
+            controllerAs: 'locationVisitButtonVm',
+            bindToController: true,
+            templateUrl: '/veganaut/location/locationVisitButton.tpl.html'
+        };
+    };
+
+    // Expose as directive
+    module.directive('vgLocationVisitButton', [locationVisitButtonDirective]);
+})();
