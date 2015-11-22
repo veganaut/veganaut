e19dbe835171f321824b476c757fb5eac8c2595d
Tobias Leugger <leugger.tobias@web.de>
2015-11-23 00:18:47 +0100

    Getting the location list to a usable state: - Loading location details when opening a location - Using all the newly created standard directives to show location name, icons, description and products. - Showing radius within which locations are shown - Validating radius and coordinates - Adding translations (well, sort of for French)

--- app/veganaut/location/locationList.js
+++ app/veganaut/location/locationList.js
@@ -1,4 +1,4 @@
-(function(module) {
+(function() {
     'use strict';
 
     /**
@@ -11,56 +11,106 @@
             scope: {
                 _locations: '=vgLocations'
             },
-            controller: ['$scope', '$location', 'locationService', function($scope, $location, locationService) {
-                var vm = this;
+            controller: [
+                '$scope', '$location', 'locationService',
+                function($scope, $location, locationService) {
+                    var vm = this;
 
-                // TODO: document
-                // TODO: make a directive for the actual list and open/closing
+                    /**
+                     * Sorted list of locations to show
+                     * @type {Array}
+                     */
+                    vm.list = [];
 
-                vm.list = [];
+                    /**
+                     * Whether we have a valid query (center and radius)
+                     * @type {boolean}
+                     */
+                    vm.validQuery = false;
 
-                vm.openLocationId = undefined;
+                    /**
+                     * Radius of this query formatted for display
+                     * @type {string}
+                     */
+                    vm.displayRadius = '';
 
-                vm.toggleOpen = function(location) {
-                    if (vm.openLocationId === location.id) {
-                        vm.openLocationId = undefined;
-                        //angularPiwik.track('location.products', 'close');
+                    /**
+                     * Handler for toggling the open state of a location in the list
+                     * @param {Location} location
+                     * @param {boolean} isOpen
+                     */
+                    vm.onOpenToggle = function(location, isOpen) {
+                        if (isOpen) {
+                            // Activate the location (this will load the full location data)
+                            locationService.activate(location);
+                        }
+                    };
+
+                    /**
+                     * Sorts and filters the locations and stores them in vm.list
+                     * @param {Location[]} locations
+                     */
+                    var compileList = function(locations) {
+                        vm.list = _.chain(locations)
+                            .filter(function(loc) {
+                                return !loc.isDisabled();
+                            })
+                            .sortByOrder(['quality.average', 'quality.numRatings', 'name'], ['desc', 'desc', 'asc'])
+                            // TODO: should sort by rank, but don't have the rank in the frontend
+                            .value()
+                        ;
+                    };
+
+                    // Watch the locations to re-compile the list on changes
+                    $scope.$watchCollection('locationListVm._locations', compileList);
+
+                    // Parse query
+                    // TODO: the whole parsing should happen in its own function or so
+                    var query = $location.search();
+                    var lat = parseFloat(query.lat);
+                    var lng = parseFloat(query.lng);
+                    var radius = parseInt(query.radius, 10);
+
+                    // Check if valid
+                    if (!isNaN(lat) && !isNaN(lng) && !isNaN(radius)) {
+                        vm.validQuery = true;
+
+                        // Valid, get the locations
+                        locationService.getLocationsByRadius(lat, lng, radius).then(compileList);
+                    }
+
+                    // Round the radius to two significant digits and display it as meters or kms
+                    // TODO: this should be a filter
+                    var roundingHelper = Math.pow(10, ('' + radius).length) / 100;
+                    var roundedRadius = Math.round(radius / roundingHelper) * roundingHelper;
+                    if (roundedRadius < 1000) {
+                        vm.displayRadius = roundedRadius + 'm';
                     }
                     else {
-                        vm.openLocationId = location.id;
-                        //angularPiwik.track('location.products', 'open');
+                        vm.displayRadius = (roundedRadius / 1000) + 'km';
                     }
-                };
 
-                // TODO: de-duplicate with MapCtrl
-                vm.visitLocation = function(location) {
-                    $location.path('location/' + location.id);
-                };
-
-                function compileList(locations) {
-                    vm.list = _.chain(locations)
-                        .filter(function(loc) {
-                            return !loc.isDisabled();
-                        })
-                        .sortByOrder(['quality.average', 'quality.numRatings', 'name'], ['desc', 'desc', 'asc'])
-                        // TODO: should sort by rank
-                        .value();
+                    // Reset search parameters when navigating away from this page
+                    // TODO: should probably not use $onRootScope
+                    $scope.$onRootScope('$routeChangeStart', function(event) {
+                        if (!event.defaultPrevented) {
+                            $location
+                                .search('lat', null)
+                                .search('lng', null)
+                                .search('radius', null)
+                            ;
+                        }
+                    });
                 }
-
-                $scope.$watchCollection('locationListVm._locations', compileList);
-
-
-                // TODO: validate input from search params
-                var query = $location.search();
-                locationService.getLocationsByRadius(query.lat, query.lng, query.radius).then(compileList);
-
-            }],
+            ],
             controllerAs: 'locationListVm',
             bindToController: true,
-            templateUrl: '/veganaut/location/vgLocationList.tpl.html'
+            templateUrl: '/veganaut/location/locationList.tpl.html'
         };
     };
 
     // Expose as directive
-    module.directive('vgLocationList', [locationListDirective]);
+    angular.module('veganaut.app.location')
+        .directive('vgLocationList', [locationListDirective])
+    ;
 })(window.veganaut.locationModule);
