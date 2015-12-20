53aec701839c7f71872e2e1bac699a85c59953d8
Tobias Leugger <leugger.tobias@web.de>
2015-12-20 16:12:17 +0100

    Removing obsolte e2e test helper method.

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -14,37 +14,6 @@ var helpers = {
     GRAPH_DELAY: 800,
 
     /**
-     * Helper for selecting an item in a drop down by text
-     * Taken from https://coderwall.com/p/tjx5zg
-     * @param selector
-     * @param item
-     */
-    selectOption: function(selector, item) {
-        var selectList, desiredOption;
-
-        selectList = browser.findElement(selector);
-        selectList.click();
-
-        selectList.findElements(by.tagName('option'))
-            .then(function findMatchingOption(options) {
-                options.some(function(option) {
-                    option.getText().then(function(text) {
-                        if (item === text) {
-                            desiredOption = option;
-                            return true;
-                        }
-                    });
-                });
-            })
-            .then(function() {
-                if (desiredOption) {
-                    desiredOption.click();
-                }
-            })
-        ;
-    },
-
-    /**
      * Adds style rules for easier testing
      */
     loadTestStylesheet: function() {
