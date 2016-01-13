5c9610e25ea60fac9fd2e983757338c7009ac1a3
Tobias Leugger <leugger.tobias@web.de>
2016-01-13 15:00:47 +0100

    Starting to refactor the map page by adding an overlay directive that holds the fitlers, product list, ...

---
+++ app/components/ui/overlay/overlay.js
@@ -0,0 +1,35 @@
+(function() {
+    'use strict';
+
+    // Part of the ui module
+    var module = angular.module('veganaut.ui');
+
+    /**
+     * Layout directive for showing content directly below the nav bar
+     * in an overlay above other content.
+     * TODO: expand this directive to be more versatile (at close button, title, ...)
+     *
+     * @returns {directive}
+     *
+     * @example
+     * <vg-overlay>
+     *     Overlay content
+     * </vg-overlay>
+     */
+    var overlayDirective = function() {
+        return {
+            restrict: 'E',
+            transclude: true,
+            scope: {},
+
+            controller: angular.noop,
+            controllerAs: 'overlayVm',
+            bindToController: true,
+            // TODO: style "overlay" directly instead of using classes?
+            template: '<div class="overlay top-box container main-container" ng-transclude></div>'
+        };
+    };
+
+    // Expose as directive
+    module.directive('vgOverlay', [overlayDirective]);
+})();
