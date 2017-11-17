8164974daf9d520665d959d3ecef0b616a3d9541
Mak <mak@nothing.ch>
2017-11-17 17:18:29 +0100

    feat(List): use the areaList to show the restaurants and shops lists

--- app/veganaut/list/listComponent.js
+++ app/veganaut/list/listComponent.js
@@ -18,11 +18,12 @@
     }
 
     ListController.$inject = [
-        '$routeParams',
-        'locationFilterService'
+        'backendService',
+        'locationFilterService',
+        'locationService'
     ];
 
-    function ListController($routeParams, locationFilterService) {
+    function ListController(backendService, locationFilterService, locationService) {
         var $ctrl = this;
 
         $ctrl.typeFilters = [
@@ -36,12 +37,79 @@
             ]
         ];
 
-        $ctrl.selectedType = $routeParams.type;
+        locationFilterService.setFiltersFromUrl();
+        $ctrl.selectedType = locationFilterService.activeFilters.type;
 
         $ctrl.$onInit = function() {
+            switch (locationFilterService.activeFilters.type) {
+            case 'gastronomy':
+                $ctrl.listName = 'locationList';
+                $ctrl.onOpenToggle = locationService.loadFullLocation;
+                $ctrl.onLoadItems = function(lat, lng, radius, limit, skip, addressType) {
+                    return locationService.getLocationsByRadius(lat, lng, radius, limit, skip, addressType)
+                        .then(function(data) {
+                            return {
+                                totalItems: data.totalLocations,
+                                items: data.locations
+                            };
+                        });
+                };
+                break;
+            case 'retail':
+                $ctrl.listName = 'locationList';
+                $ctrl.onOpenToggle = locationService.loadFullLocation;
+                $ctrl.onLoadItems = function(lat, lng, radius, limit, skip, addressType) {
+                    return locationService.getLocationsByRadius(lat, lng, radius, limit, skip, addressType)
+                        .then(function(data) {
+                            return {
+                                totalItems: data.totalLocations,
+                                items: data.locations
+                            };
+                        });
+                };
+                break;
+            case 'meals':
+                $ctrl.listName = 'productList';
+                // $ctrl.onOpenToggle =
+                $ctrl.onLoadItems = function(lat, lng, radius, limit, skip) {
+                    return backendService.getProducts(
+                        lat, lng, radius,
+                        locationFilterService.getTypeFilterValue(),
+                        skip, limit
+                    ).then(function(res) {
+                        return {
+                            totalItems: res.data.totalProducts,
+                            items: res.data.products
+                        };
+                    });
+                };
+                break;
+            case 'products':
+                $ctrl.listName = 'productList';
+                // $ctrl.onOpenToggle =
+                $ctrl.onLoadItems = function(lat, lng, radius, limit, skip) {
+                    return backendService.getProducts(
+                        lat, lng, radius,
+                        locationFilterService.getTypeFilterValue(),
+                        skip, limit
+                    ).then(function(res) {
+                        return {
+                            totalItems: res.data.totalProducts,
+                            items: res.data.products
+                        };
+                    });
+                };
+                break;
+            default:
+                // If no type set, switch to gastronomy by default
+                $ctrl.setTypeFilter('gastronomy');
+                // This makes the component to be loaded twice. TODO: Find a better way to do it.
+                break;
+            }
         };
 
         $ctrl.setTypeFilter = function(type) {
+            $ctrl.selectedType = type;
             locationFilterService.activeFilters.type = type;
             locationFilterService.onFiltersChanged();
         };
