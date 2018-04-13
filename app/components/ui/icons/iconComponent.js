22024a0316985318e7563cd7bb2a8ec9ef58b5b8
Tobias Leugger <leugger.tobias@web.de>
2018-04-13 17:10:07 +0200

    Clean up iconComponent and make it respond to changes in input correctly.

--- app/components/ui/icons/iconComponent.js
+++ app/components/ui/icons/iconComponent.js
@@ -8,27 +8,16 @@
     function iconComponent() {
         var component = {
             bindings: {
-                name: '<vgName',
-                label: '<?vgLabel'
+                name: '<vgName'
             },
             controller: IconComponentController,
-            controllerAs: 'vm',
+            controllerAs: '$ctrl',
             templateUrl: 'components/ui/icons/iconComponent.html'
         };
 
         return component;
     }
 
-    // Inject dependencies
-    // iconComponentController.$inject = ['exampleService'];
-
     function IconComponentController() {
-        var vm = this;
-
-        vm.iconPath = null;
-
-        vm.$onInit = function() {
-            vm.iconPath = '/components/ui/icons/assets/' + vm.name + '.svg';
-        };
     }
 })();
