a5dcb51e05603114af0dcfe782d4a07403195b09
Tobias Leugger <leugger.tobias@web.de>
2018-09-05 16:55:41 +0200

    Redirecting to register when accessing a user page.

--- app/veganaut/user/PersonCtrl.js
+++ app/veganaut/user/PersonCtrl.js
@@ -3,6 +3,10 @@
 
     module.controller('PersonCtrl', ['$scope', '$routeParams', 'backendService',
         function($scope, $routeParams, backendService) {
+            if (!backendService.isLoggedIn()) {
+                $scope.goToView('/register');
+            }
+
             var personId = $routeParams.personId;
             backendService.getPerson(personId).success(function(person) {
                 $scope.person = person;
